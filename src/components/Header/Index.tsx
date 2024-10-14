6b886776a8dfcde7a28a2bc8a162efa18ecde31b
gss-patricia <gss.patricia@gmail.com>
2024-10-14 22:55:36 +0100

    video 1.3

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -7,6 +7,7 @@ import Input from "../Input";
 import { SearchIcon } from "../../common/icons/SearchIcon";
 import { Link } from "react-router-dom";
 import Typography from "../Typography";
+import Button from "../Button";
 
 type HeaderProps = {
   cartCount: number;
@@ -50,9 +51,13 @@ const Header = ({ cartCount, onSearch }: HeaderProps) => {
               placeholder="O que você procura?"
               icon={<SearchIcon />}
             />
-            <button onClick={handleSearch}>
-              <i className="fa fa-search"></i>
-            </button>
+            <Button
+              style={{ position: "relative", right: "36px" }}
+              variant="secondary"
+              onClick={() => {}}
+            >
+              <SearchIcon />
+            </Button>
           </div>
           <div className={Styles.userActions}>
             <a href="#">Login</a>
