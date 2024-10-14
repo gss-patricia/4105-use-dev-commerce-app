e2bbeefabddd2498b5ea9ec34bfd57cf08e5cd94
gss-patricia <gss.patricia@gmail.com>
2024-10-14 23:21:58 +0100

    video 2.2

---
+++ src/components/SimpleBanner/index.tsx
@@ -0,0 +1,5 @@
+import HeroBanner, { HeroBannerProps } from "../HeroBanner";
+const SimpleBanner = ({ backgroundImage }: HeroBannerProps) => (
+  <HeroBanner backgroundImage={backgroundImage} />
+);
+export default SimpleBanner;
